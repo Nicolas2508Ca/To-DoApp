56859a38fd4d9a33cf19478d5a13923287a6d94f
Nicolas <davidnicolas431@gmail.com>
2024-01-16 00:35:36 -0500

    Creacion de Title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import {Title} from "./componentes/Title";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-6xl">Todo App</h1>
+    <div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-col max-w-xl">
+        <Title />
+      </div>
     </div>
   );
 }
