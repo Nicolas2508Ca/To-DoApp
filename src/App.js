9203af3641ceab6c9f73d369e5123472f520664c
Nicolas <davidnicolas431@gmail.com>
2024-01-16 13:06:49 -0500

    Elaboracion del Input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Title} from "./componentes/Title";
+import { ToDoInput } from "./componentes/ToDoInput";
 
 function App() {
   return (
     <div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
+        <ToDoInput />
       </div>
     </div>
   );
