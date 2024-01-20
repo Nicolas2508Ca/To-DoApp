0998402c6bb05a4e2e22cfc280036dda39aae4c6
Nicolas <davidnicolas431@gmail.com>
2024-01-20 15:48:48 -0500

    Creacion de los To Do

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Title } from "./componentes/Title";
-import { ToDoInput } from "./componentes/ToDoInput";
+import { TodoInput } from "./componentes/ToDoInput";
 import { ToDoList } from "./componentes/ToDoList";
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,11 +27,24 @@ function App() {
     }
   ])
 
+  const addTodo = (title) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false,
+    };
+
+    const todoList = [...todos]
+    todoList.push(newTodo);
+    setTodos(todoList);
+  }
+
   return (
     <div className="bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
-        <ToDoInput />
+        <TodoInput addTodo={addTodo} />
         <ToDoList todos={todos} />
       </div>
     </div>
--- src/componentes/ToDo/ToDo.jsx
+++ src/componentes/ToDo/ToDo.jsx
@@ -1,11 +1,20 @@
 const ToDo = ({ todo }) => {
+
+    const { title, completed } = todo
     return(
         <div className="flex items-center justify-between p-4 bg bg-gray-700 border-b  border-solid border-gray-600">
             <div className="flex items-center">
-                <span className="border-solid border border-gray-500 rounded-full p-3 cursor-pointer">
-                </span>
-                <p className="pl-3">
-                    {todo.title}
+                {
+                    completed ? (
+                        <div className="bg-green-700 p-1 rounded-full cursor-pointer">
+                            <img className="h-4 w-4" src="/check-icon.svg" alt="check Icon"></img>
+                        </div>
+                    ) : (
+                        <span className="border-solid border border-gray-500 rounded-full p-3 cursor-pointer"></span>
+                    )
+                }
+                <p className={"ml-3 pl-3" + (completed && "line-through")}>
+                    {title}
                 </p>
             </div>
             <img className="h-5 w-5 cursor-pointer transition-all duration-300 ease-in-out" src="/close-icon.svg" alt="close Icon"></img>
